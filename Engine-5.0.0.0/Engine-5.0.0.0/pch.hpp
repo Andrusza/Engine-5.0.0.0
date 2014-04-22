8703ca46852e937b9ff66e1f11467ef4332685be
Andrusza <andrusza1989@gmail.com>
2014-04-22 16:26:33 +0200

    project conf

--- Engine-5.0.0.0/Engine-5.0.0.0/pch.hpp
+++ Engine-5.0.0.0/Engine-5.0.0.0/pch.hpp
@@ -1,3 +1,31 @@
 #pragma once
+#pragma warning(disable:4996)
 
+//C++
 #include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <map>
+#include <sstream>
+#include <iomanip>
+
+//YAML
+#include "ExternalLibraries\yaml-cpp\include\yaml-cpp\yaml.h"
+
+//OpenGL
+#include "ExternalLibraries\glew-1.9.0\include\GL\glew.h"
+#include "ExternalLibraries\freeglut\include\GL\freeglut.h"
+
+//FreeImage
+#include "ExternalLibraries\FreeImage\FreeImage.h"
+
+//GLM
+#include "ExternalLibraries\glm-0.9.4.3\glm\glm.hpp"
+#include "ExternalLibraries\glm-0.9.4.3\glm\gtc\matrix_transform.hpp"
+#include "ExternalLibraries\glm-0.9.4.3\glm\gtc\quaternion.hpp"
+#include "ExternalLibraries\glm-0.9.4.3\glm\gtx\euler_angles.hpp"
+#include "ExternalLibraries\glm-0.9.4.3\glm\gtc\matrix_transform.hpp"
