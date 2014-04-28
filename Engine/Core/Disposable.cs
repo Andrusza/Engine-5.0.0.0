9eed8f2ac1aaa6cd0c0cc6725359f35393b10567
Andrusza <andrusza1989@gmail.com>
2014-04-28 13:04:16 +0200

    Disposable

---
+++ Engine/Core/Disposable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OpenGlobe.Core
+{
+    public abstract class Disposable : IDisposable
+    {
+        private bool _disposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        protected abstract void ReleaseManagedResources();
+
+        private void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                ReleaseManagedResources();
+            }
+
+            _disposed = true;
+        }
+    }
+}
